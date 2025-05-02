ffbc9c88e81761b74d039f9d017381f6e9d0ae4a
Folami <folami.bidmus@gmail.com>
2025-05-02 17:37:09 +0100

    [Parse API Version]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,12 +15,12 @@ public class Main {
         int port = 9092;
         try {
             serverSocket = new ServerSocket(port);
-            // Set SO_REUSEADDR so that we don't get 'Address already in use' errors.
+            // Set SO_REUSEADDR to avoid 'Address already in use' errors.
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
             System.err.println("Connection from " + clientSocket.getRemoteSocketAddress() + " has been established!");
-
+            
             // Read the first 12 bytes from the request:
             // - 4 bytes: message_size (we don't use this value).
             // - Next, request header v2:
@@ -45,23 +45,31 @@ public class Main {
                 return;
             }
             
-            // Unpack the 12 bytes. Set byte order to big-endian.
+            // Unpack the 12 bytes in big-endian order:
+            // Format: INT32, INT16, INT16, INT32
             ByteBuffer reqBuffer = ByteBuffer.wrap(reqHeader);
             reqBuffer.order(ByteOrder.BIG_ENDIAN);
             int requestMessageSize = reqBuffer.getInt(); // message_size (not used)
             short requestApiKey = reqBuffer.getShort();
             short requestApiVersion = reqBuffer.getShort();
             int correlationId = reqBuffer.getInt();
-            System.err.println("Received correlation_id: " + correlationId);
+            System.err.println("Received correlation_id: " + correlationId +
+                               ", requested api_version: " + requestApiVersion);
+            
+            // Determine error_code:
+            // If the requested ApiVersions request version is unsupported (broker supports versions 0 to 4),
+            // then error_code should be set to 35 ("UNSUPPORTED_VERSION"); otherwise, 0.
+            short errorCode = (requestApiVersion < 0 || requestApiVersion > 4) ? (short)35 : (short)0;
             
             // Build response:
             // - message_size: 4 bytes (any value works; we'll use 0)
-            // - correlation_id: parsed from the request header.
-            // All integers are 32-bit signed in big-endian order.
-            ByteBuffer buffer = ByteBuffer.allocate(8);
+            // - correlation_id: from the request header (4 bytes)
+            // - error_code: 2 bytes (16-bit signed integer)
+            ByteBuffer buffer = ByteBuffer.allocate(10);
             buffer.order(ByteOrder.BIG_ENDIAN);
             buffer.putInt(0);             // message_size (placeholder)
-            buffer.putInt(correlationId);  // correlation_id from request
+            buffer.putInt(correlationId);   // correlation_id
+            buffer.putShort(errorCode);     // error_code
             byte[] response = buffer.array();
             
             // Send response.
